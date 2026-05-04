bbd707eedad66953ad7c87ef4216736ebf6d8d1a
Cătălin Toma <catalin@eidkit.ro>
2026-05-04 13:06:52 +0300

    Bump version for first prod release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "ro.eidkit.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.1.4"
+        versionCode = 9
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
