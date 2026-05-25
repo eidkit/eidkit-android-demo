ea35c0c41f5bbb52a3ecd631bb444cdd7cd4ce74
Cătălin Toma <catalin@eidkit.ro>
2026-05-25 17:50:57 +0300

    Bump versionCode to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "ro.eidkit.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
+        versionCode = 21
         versionName = "1.10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
